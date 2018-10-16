71940512e68a35862302b2e1c5801b527f5b4319
Elena Melnikova <elenamelnikova@owners-macbook-pro.local>
2018-10-16 14:09:43 -0400

    Initial Commit

---
+++ MAPD714_ElenaMelnikova_Tab_Bar_Calculator/ScientificCalculatorViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ScientificCalculatorViewController.swift
+//  MAPD714_ElenaMelnikova_Tab_Bar_Calculator
+//
+//  Created by Elena Melnikova on 2018-10-16.
+//  Copyright © 2018 Centennial College. All rights reserved.
+//
+
+import UIKit
+
+class ScientificCalculatorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
